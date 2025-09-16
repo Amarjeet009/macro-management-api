6864d2c8c2afeaca331567be5e7844ff7f2ff7f0
Amarjeet Rao <amarjeetrao@outlook.com>
2025-09-17 00:44:04 +0530

    quick starter template update

---
+++ src/test/kotlin/com/amarj/MacroManagementApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amarj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MacroManagementApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
